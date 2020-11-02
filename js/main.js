5ffb3aa4326338cee49b9fa77b97b49c976134b8
Vinicius Teixeira <vttsouza@gmail.com>
2020-11-02 08:26:55 -0300

    Adicionando comida e a deixando aleatória

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen"
@@ -18,6 +22,11 @@ function criarCobrinha() {
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function criarComida() {
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
 }
 
 function update(event) {
@@ -35,9 +44,9 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if (snake[0].y < 0 && direction == "up") snake[0].y = 15 * box
 
-
     criarBG()
     criarCobrinha()
+    criarComida()
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
