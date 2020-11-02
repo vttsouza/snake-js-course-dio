0c29fc35bf821a74f11d45d3e419d3ecfe3bdeb3
Vinicius Teixeira <vttsouza@gmail.com>
2020-11-02 08:34:46 -0300

    Aumentando o tamanho da cobrinha e mudando fruta de lugar ao ser comida

--- js/main.js
+++ js/main.js
@@ -56,7 +56,14 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box
     if (direction == "down") snakeY += box
 
-    snake.pop()
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop()
+    } else {
+        food = {
+            x: Math.floor(Math.random() * 15 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 1) * box
+        }
+    }
 
     let novaCabeca = {
         x: snakeX,
